ef44679cd6b395d81cf48506f04086e5f5b4bc89
sudipto <sudipta.shahin@dreamonline.co.jp>
2025-08-07 14:52:35 +0600

    add snippet app dive in serializers

--- jwt_auth_demo/snippets/serializers.py
+++ jwt_auth_demo/snippets/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet
+# , LANGUAGE_CHOICES, STYLE_CHOICES
 # from django.contrib.auth import get_user_model
 from django.http import HttpRequest
 from typing import TypedDict, Optional
@@ -20,19 +21,27 @@ class SnippetData(TypedDict, total=False):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username"]
+        # here we use tuple `()`` instead of `[]` because they are not going
+        # change dynamically
+        # fields = ["id", "username"]
+        fields = ("id", "username")
 
 
-class SnippetModelSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.ModelSerializer):
     owner = UserSerializer(read_only=True)
     title = serializers.CharField(required=True, max_length=100)
 
     class Meta:
         model = Snippet
         # fields = '__all__'
-        fields = ["id", "title", "code", "lineos", "language", "style",
-                  "owner"]
-        read_only_fields = ["id", "owner"]
+        # fields = ["id", "title", "code", "lineos", "language", "style",
+        #           "owner"]
+        # here we use tuple `()`` instead of `[]` because they are not going
+        # change dynamically
+        fields = ("id", "title", "code", "lineos", "language", "style",
+                  "owner")
+        # read_only_fields = ["id", "owner"]
+        read_only_fields = ("id", "owner")
 
     # field level validation
     def validate_title(self, value: str) -> str:
@@ -66,34 +75,36 @@ class SnippetModelSerializer(serializers.ModelSerializer):
         return super().update(instance, validated_data)
 
 
-class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(
-        required=False, alow_blank=True, max_length=100
-    )
-    code = serializers.CharField(style={"base_template": "textarea.html"})
-    lineos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(
-        choices=LANGUAGE_CHOICES, default="python"
-    )
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default="friendly")
+# class SnippetSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(
+#         required=False, alow_blank=True, max_length=100
+#     )
+#     code = serializers.CharField(style={"base_template": "textarea.html"})
+#     lineos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(
+#         choices=LANGUAGE_CHOICES, default="python"
+#     )
+#     style = serializers.ChoiceField(
+#     choices=STYLE_CHOICES, default="friendly"
+# )
 
-    def create(self, validated_data: SnippetData) -> Snippet:
-        """
-        Create and return a new `Snippet` instance, given the validated_data
-        """
-        return Snippet.objects.create(**validated_data)
+#     def create(self, validated_data: SnippetData) -> Snippet:
+#         """
+#         Create and return a new `Snippet` instance, given the validated_data
+#         """
+#         return Snippet.objects.create(**validated_data)
 
-    def updated(self, instance: Snippet,
-                validated_data: SnippetData) -> Snippet:
-        """
-        Update and return an existing `Snippet` instance, given the validated
-        data.
-        """
-        instance.title = validated_data.get("title", instance.title)
-        instance.code = validated_data.get("code", instance.code)
-        instance.lineos = validated_data.get("lineos", instance.lineos)
-        instance.language = validated_data.get("language", instance.language)
-        instance.style = validated_data.get("style", instance.style)
-        instance.save()
-        return instance
+#     def updated(self, instance: Snippet,
+#                 validated_data: SnippetData) -> Snippet:
+#         """
+#         Update and return an existing `Snippet` instance, given the validated
+#         data.
+#         """
+#         instance.title = validated_data.get("title", instance.title)
+#         instance.code = validated_data.get("code", instance.code)
+#         instance.lineos = validated_data.get("lineos", instance.lineos)
+#         instance.language = validated_data.get("language", instance.language)
+#         instance.style = validated_data.get("style", instance.style)
+#         instance.save()
+#         return instance
